990bd2ae6fd46433562d07cdff747efe1581db07
Arseny <Syp4ik22885@mail.ru>
2023-02-21 19:35:15 +0300

    init ci1

--- Seminar_01.py
+++ Seminar_01.py
@@ -1,9 +1,30 @@
 "1. Напишите программу, которая принимает на вход два числа и проверяет, является ли одно число квадратом другого."
 
 "Это сделал я "  "** 2 - в квадрате"
-number1 = int(input())
-number2 = int(input())
-if number2 == number1 ** 2 or number1 == number2 ** 2:
-    print('Yes')
-else:
-    print('No')
+# number1 = int(input())
+# number2 = int(input())
+# if number2 == number1 ** 2 or number1 == number2 ** 2:
+#    print('Yes')
+# else:
+#    print('No')
+
+"2. Напишите программу, которая на вход принимает 5 чисел и находит максимальное из них."
+
+# some_list = []
+# for _ in range(5):  # 0, 1, 2, 3, 4 искуственно создали последовательность "_"
+#     x = int(input())
+#     some_list.append(x)  # внести в список х
+
+# print(max(some_list))
+
+# maxx = some_list[0] # Описание print(max(some_list))
+# for el in some_list:
+#     if el > maxx:
+#         maxx = el
+# print(maxx)
+
+# maxx = some_list[0]  # Это у нас решение через индекс
+# for ind in range(5):
+#     if some_list[ind] > maxx:
+#         maxx = some_list[ind]
+# print(maxx)

62c1d1c8763a23212b8d936aa55a2adce2a4e609
Arseny <Syp4ik22885@mail.ru>
2023-02-26 23:46:09 +0300

    end

--- Seminar_01.py
+++ Seminar_01.py
@@ -28,3 +28,42 @@
 #     if some_list[ind] > maxx:
 #         maxx = some_list[ind]
 # print(maxx)
+
+""""Параметры sep, end"""
+
+# print(7, end=', ')
+# print(8)
+# print(7, 8, 9, sep=', ')
+# someList = [1, 2, 3, 4]
+# print(*someList, sep=', ')
+
+"Напишите программу, которая будет на вход принимать число N и выводить числа от -N до N"
+
+# x = int(input())
+# for i in range(-x, x + 1):
+#     print(i, end=', ')
+
+"Напишите программу, которая будет принимать на вход дробь и показывать первую цифру дробной части числа."
+
+# x = input()
+# pos = x.find(',')
+# if ',' not in x:
+#     print('нет')
+# else:
+#     print(x[pos + 1])
+
+
+# a = input()
+# if '.' in a:
+# ind = a.index('.')
+# print(a[ind + 1])
+# else:
+# print('нет')
+
+"Напишите программу, которая принимает на вход число и проверяет, кратно ли оно 5 и 10 или 15, но не 30."
+
+# x = int(input())
+# if (x % 5 == 0 and x % 10 == 0 or x % 15 == 0) and x % 30 != 0:
+#     print('da')
+# else:
+#     print('net')
